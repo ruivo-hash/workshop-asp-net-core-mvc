2c3237d044d0705b060098659ddb06373cfb47a1
ruivo-hash <maateusb123@gmail.com>
2023-03-28 12:36:34 -0300

    Other entities and second migration

--- WebSalesMvc/Data/WebSalesMvcContext.cs
+++ WebSalesMvc/Data/WebSalesMvcContext.cs
@@ -14,6 +14,8 @@ namespace WebSalesMvc.Data
         {
         }
 
-        public DbSet<WebSalesMvc.Models.Department> Department { get; set; } = default!;
+        public DbSet<Department> Department { get; set; } = default!;
+        public DbSet<Seller> Seller { get; set; } = default!;
+        public DbSet<SalesRecord> SalesRecord { get; set; } = default!;
     }
 }
