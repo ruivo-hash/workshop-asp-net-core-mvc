6da515c60b76d17054c4563145956ed838ce4fcb
ruivo-hash <maateusb123@gmail.com>
2023-03-29 17:08:23 -0300

    Validation

--- WebSalesMvc/Models/Seller.cs
+++ WebSalesMvc/Models/Seller.cs
@@ -5,16 +5,28 @@ namespace WebSalesMvc.Models
     public class Seller
     {
         public int Id { get; set; }
+
+        [Required(ErrorMessage = "{0} required")]
+        [StringLength(60, MinimumLength = 3, ErrorMessage = "{0} size should be between {2} and {1}")]// o 0 pega o nome do campo e a partir do 1 são os valores passados 1 = 60/2 = 3
         public string Name { get; set; }
+
         [DataType(DataType.EmailAddress)]
+        [Required(ErrorMessage = "{0} required")]
+        [EmailAddress(ErrorMessage = "Enter a valid {0}")]
         public string Email { get; set; }
-        [Display(Name = "Birth Date")]
+
         [DataType(DataType.Date)]
+        [Display(Name = "Birth Date")]
+        [Required(ErrorMessage = "{0} required")]
         [DisplayFormat(DataFormatString = "{0:dd/MM/yyyy}")]
         public DateTime BirthDate { get; set; }
+
         [Display(Name = "Salary")]
+        [Required(ErrorMessage = "{0} required")]
         [DisplayFormat(DataFormatString = "{0:F2}")] // o {0} indica o valor da propriedade
+        [Range(100.0, 50000.0, ErrorMessage = "{0} must be from {1} to {2}")]
         public double BaseSalary { get; set; }
+
         public Department Department { get; set; }
         public int DepartmentId { get; set; }
         public ICollection<SalesRecord> Sales { get; set; } = new List<SalesRecord>();
