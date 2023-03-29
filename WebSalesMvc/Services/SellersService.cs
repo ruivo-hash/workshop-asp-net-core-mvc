9353bd382be2b28c916e81bfbd380887b2a663f6
ruivo-hash <maateusb123@gmail.com>
2023-03-29 00:19:03 -0300

    Seller details and eager loading

--- WebSalesMvc/Services/SellersService.cs
+++ WebSalesMvc/Services/SellersService.cs
@@ -1,4 +1,5 @@
-﻿using WebSalesMvc.Data;
+﻿using Microsoft.EntityFrameworkCore;
+using WebSalesMvc.Data;
 using WebSalesMvc.Models;
 
 namespace WebSalesMvc.Services
@@ -25,7 +26,7 @@ namespace WebSalesMvc.Services
 
         public Seller FindById(int id)
         {
-            return _context.Seller.FirstOrDefault(obj => obj.Id == id);
+            return _context.Seller.Include(obj => obj.Department).FirstOrDefault(obj => obj.Id == id);
         }
         public void Remove(int id)
         {
