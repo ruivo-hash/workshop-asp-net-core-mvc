605dd04c93b8a3498393f96bcc4a983c15e1a4de
ruivo-hash <maateusb123@gmail.com>
2023-03-29 23:38:05 -0300

    Implementing grouping search

--- WebSalesMvc/Services/SalesRecordService.cs
+++ WebSalesMvc/Services/SalesRecordService.cs
@@ -30,5 +30,39 @@ namespace WebSalesMvc.Services
                 .OrderByDescending(x => x.Date)
                 .ToListAsync();
         }
+
+        //public List<IGrouping<Department, SalesRecord>> FindByDateGrouping(DateTime? minDate, DateTime? maxDate)
+        //{
+        //    var result = from obj in _context.SalesRecord select obj;
+        //    if (minDate.HasValue)
+        //    {
+        //        result = result.Where(x => x.Date >= minDate.Value);
+        //    }
+        //    if (maxDate.HasValue)
+        //    {
+        //        result = result.Where(x => x.Date <= maxDate.Value);
+        //    }
+        //    return result.Include(x => x.Seller).Include(x => x.Seller.Department).OrderByDescending(x => x.Date).GroupBy(x => x.Seller.Department).ToList();
+        //}
+
+        public async Task<List<IGrouping<Department, SalesRecord>>> FindByDateGroupingAsync(DateTime? minDate, DateTime? maxDate)
+        {
+            var result = from obj in _context.SalesRecord select obj;
+            if (minDate.HasValue)
+            {
+                result = result.Where(x => x.Date >= minDate.Value);
+            }
+            if (maxDate.HasValue)
+            {
+                result = result.Where(x => x.Date <= maxDate.Value);
+            }
+            var data = await result
+                .Include(x => x.Seller)
+                .Include(x => x.Seller.Department)
+                .OrderByDescending(x => x.Date)
+                .ToListAsync();
+
+            return data.GroupBy(x => x.Seller.Department).ToList();
+        }
     }
 }
